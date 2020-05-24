4622c76c4874f3cd1a56ed0e2df51f66c2d2f7d5
jawaabu <49921621+jawabuu@users.noreply.github.com>
2020-05-24 16:21:44 +0300

    verify digitalocean provider functionality

--- main.tf
+++ main.tf
@@ -1,123 +1,42 @@
-module "provider" {
-  source = "./provider/hcloud"
-
-  token           = var.hcloud_token
-  ssh_keys        = var.hcloud_ssh_keys
-  location        = var.hcloud_location
-  type            = var.hcloud_type
-  image           = var.hcloud_image
-  hosts           = var.node_count
-  hostname_format = var.hostname_format
+module "ssh" {
+  source          = "./ssh"
+  ssh_key_path    = var.ssh_key_path
+  ssh_pubkey_path = var.ssh_pubkey_path
+  ssh_keys_dir    = var.ssh_keys_dir
 }
 
-# module "provider" {
-#   source = "./provider/scaleway"
-#
-#   organization_id = var.scaleway_organization_id
-#   access_key      = var.scaleway_access_key
-#   secret_key      = var.scaleway_secret_key
-#   zone            = var.scaleway_zone
-#   type            = var.scaleway_type
-#   image           = var.scaleway_image
-#   hosts           = var.node_count
-#   hostname_format = var.hostname_format
-# }
+module "provider" {
+  source = "./provider/digitalocean"
 
-# module "provider" {
-#   source = "./provider/digitalocean"
-#
-#   token           = var.digitalocean_token
-#   ssh_keys        = var.digitalocean_ssh_keys
-#   region          = var.digitalocean_region
-#   size            = var.digitalocean_size
-#   image           = var.digitalocean_image
-#   hosts           = var.node_count
-#   hostname_format = var.hostname_format
-# }
-
-# module "provider" {
-#   source = "./provider/packet"
-#
-#   auth_token       = var.packet_auth_token
-#   project_id       = var.packet_project_id
-#   billing_cycle    = var.packet_billing_cycle
-#   facility         = [var.packet_facility]
-#   plan             = var.packet_plan
-#   operating_system = var.packet_operating_system
-#   hosts            = var.node_count
-#   hostname_format  = var.hostname_format
-# }
-
-# module "provider" {
-#   source = "./provider/vsphere"
-
-#   hosts                   = var.node_count
-#   hostname_format         = var.hostname_format
-#   vsphere_server          = var.vsphere_server
-#   vsphere_datacenter      = var.vsphere_datacenter
-#   vsphere_cluster         = var.vsphere_cluster
-#   vsphere_network         = var.vsphere_network
-#   vsphere_datastore       = var.vsphere_datastore
-#   vsphere_vm_template     = var.vsphere_vm_template
-#   vsphere_vm_linked_clone = var.vsphere_vm_linked_clone
-#   vsphere_vm_num_cpus     = var.vsphere_vm_num_cpus
-#   vsphere_vm_memory       = var.vsphere_vm_memory
-#   vsphere_user            = var.vsphere_user
-#   vsphere_password        = var.vsphere_password
-# }
+  token           = var.digitalocean_token
+  ssh_keys        = var.digitalocean_ssh_keys
+  region          = var.digitalocean_region
+  size            = var.digitalocean_size
+  image           = var.digitalocean_image
+  hosts           = var.node_count
+  hostname_format = var.hostname_format
+  ssh_key_path    = module.ssh.private_key #var.ssh_key_path Override to use predefined key
+  ssh_pubkey_path = module.ssh.public_key  #var.ssh_pubkey_path Override to use predefined key
+}
 
 module "swap" {
   source = "./service/swap"
 
-  node_count  = var.node_count
-  connections = module.provider.public_ips
+  node_count   = var.node_count
+  connections  = module.provider.public_ips
+  ssh_key_path = module.ssh.private_key
 }
 
+
 module "dns" {
-  source = "./dns/cloudflare"
+  source     = "./dns/digitalocean"
 
   node_count = var.node_count
-  email      = var.cloudflare_email
-  api_token  = var.cloudflare_api_token
+  token      = var.digitalocean_token
   domain     = var.domain
   public_ips = module.provider.public_ips
   hostnames  = module.provider.hostnames
 }
-
-# module "dns" {
-#   source = "./dns/aws"
-#
-#   node_count      = var.node_count
-#   access_key = var.aws_access_key
-#   secret_key = var.aws_secret_key
-#   region     = var.aws_region
-#   domain     = var.domain
-#   public_ips = module.provider.public_ips
-#   hostnames  = module.provider.hostnames
-# }
-
-# module "dns" {
-#   source = "./dns/google"
-#
-#   node_count        = var.node_count
-#   project      = var.google_project
-#   region       = var.google_region
-#   creds_file   = var.google_credentials_file
-#   managed_zone = var.google_managed_zone
-#   domain       = var.domain
-#   public_ips   = module.provider.public_ips
-#   hostnames    = module.provider.hostnames
-# }
-
-# module "dns" {
-#   source     = "./dns/digitalocean"
-#
-#   node_count      = var.node_count
-#   token      = var.digitalocean_token
-#   domain     = var.domain
-#   public_ips = module.provider.public_ips
-#   hostnames  = module.provider.hostnames
-# }
 
 module "wireguard" {
   source = "./security/wireguard"
@@ -127,6 +46,7 @@ module "wireguard" {
   private_ips  = module.provider.private_ips
   hostnames    = module.provider.hostnames
   overlay_cidr = module.kubernetes.overlay_cidr
+  ssh_key_path = module.ssh.private_key
 }
 
 module "firewall" {
@@ -138,6 +58,7 @@ module "firewall" {
   vpn_interface        = module.wireguard.vpn_interface
   vpn_port             = module.wireguard.vpn_port
   kubernetes_interface = module.kubernetes.overlay_interface
+  ssh_key_path         = module.ssh.private_key
 }
 
 module "etcd" {
@@ -148,6 +69,7 @@ module "etcd" {
   hostnames   = module.provider.hostnames
   vpn_unit    = module.wireguard.vpn_unit
   vpn_ips     = module.wireguard.vpn_ips
+  ssh_key_path = module.ssh.private_key
 }
 
 module "kubernetes" {
@@ -159,4 +81,5 @@ module "kubernetes" {
   vpn_interface  = module.wireguard.vpn_interface
   vpn_ips        = module.wireguard.vpn_ips
   etcd_endpoints = module.etcd.endpoints
+  ssh_key_path   = module.ssh.private_key
 }
