bad08f26f48fc6ba75ce9ade95ca462e6d382527
jawaabu <49921621+jawabuu@users.noreply.github.com>
2020-06-02 23:40:19 +0300

    use overlay_cidr instead of kubernetes interface due to CNI creating multiple interfaces

--- security/ufw/scripts/ufw.sh
+++ security/ufw/scripts/ufw.sh
@@ -6,6 +6,9 @@ ufw allow ssh
 ufw allow in on ${private_interface} to any port ${vpn_port} # vpn on private interface
 ufw allow in on ${vpn_interface}
 ufw allow in on ${kubernetes_interface} # Kubernetes pod overlay interface
+## Adding this because pods in a node cannot reach vpn_ip of the same node
+## Especially necessary for CNIs that create multiple interfaces like cilium
+ufw allow from ${overlay_cidr} # Allow communication on k8s pods network
 ufw allow 6443 # Kubernetes API secure remote port
 ufw allow 80
 ufw allow 443
