8279fd8caf9db407a0d19b57da409621c015baed
jawaabu <49921621+jawabuu@users.noreply.github.com>
2020-06-07 22:00:55 +0300

    update vultr vpc assignment

--- provider/vultr/vpc.tf
+++ provider/vultr/vpc.tf
@@ -1,5 +1,5 @@
 resource "vultr_network" "kube-vpc" {
     description = "kube-vpc"
-    region_id   = vultr_region.region.id
+    region_id   = data.vultr_region.region.id
     cidr_block  = "10.115.0.0/24"
 }
