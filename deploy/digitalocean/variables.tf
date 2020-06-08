9f3854baa517f47f85b71b90bff950d8e4594537
jawaabu <49921621+jawabuu@users.noreply.github.com>
2020-06-08 21:21:05 +0300

    Add digitalocean deployment

---
+++ deploy/digitalocean/variables.tf
@@ -0,0 +1,112 @@
+/* general */
+variable "node_count" {
+  default = 3
+}
+
+/* etcd_node_count must be <= node_count; odd numbers provide quorum */
+variable "etcd_node_count" {
+  default = 3
+}
+
+variable "domain" {
+  default = "kloud3s.io"
+}
+
+variable "hostname_format" {
+  default = "kube%d"
+}
+
+variable "ssh_key_path" {
+  default = "../../.ssh/tf-kube"
+}
+
+variable "ssh_pubkey_path" {
+  default = "../../.ssh/tf-kube.pub"
+}
+
+variable "ssh_keys_dir" {
+  default = "../../.ssh"
+}
+
+variable "k3s_version" {
+  default = "latest"
+}
+
+variable "kubeconfig_path" {
+  default = "../../.kubeconfig"
+}
+
+variable "create_zone" {
+  default = false
+}
+
+variable "cni" {
+  default = "cilium"
+  description = "Choice of CNI to install e.g. flannel, weave, cilium, calico"
+}
+
+variable "overlay_cidr" {
+  default = "10.42.0.0/16"
+  description = "Cluster cidr"
+}
+
+/* digitalocean */
+variable "digitalocean_token" {
+  default = ""
+}
+
+variable "digitalocean_ssh_keys" {
+  type    = list(string)
+  default = [""]
+}
+
+variable "digitalocean_region" {
+  default = "fra1"
+}
+
+variable "digitalocean_size" {
+  default = "s-1vcpu-1gb"
+}
+
+variable "digitalocean_image" {
+  default = "ubuntu-18-04-x64"
+}
+
+/* aws dns */
+variable "aws_access_key" {
+  default = ""
+}
+
+variable "aws_secret_key" {
+  default = ""
+}
+
+variable "aws_region" {
+  default = "eu-west-1"
+}
+
+/* cloudflare dns */
+variable "cloudflare_email" {
+  default = ""
+}
+
+variable "cloudflare_api_token" {
+  default = ""
+}
+
+/* google dns */
+variable "google_project" {
+  default = ""
+}
+
+variable "google_region" {
+  default = ""
+}
+
+variable "google_managed_zone" {
+  default = ""
+}
+
+variable "google_credentials_file" {
+  default = ""
+}
