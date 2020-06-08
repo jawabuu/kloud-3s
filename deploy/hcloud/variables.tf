db25fa247641073bdcf1291ef71f6e1898bfe074
jawaabu <49921621+jawabuu@users.noreply.github.com>
2020-06-08 20:39:16 +0300

    add hcloud deployment

---
+++ deploy/hcloud/variables.tf
@@ -0,0 +1,117 @@
+/* general */
+variable "node_count" {
+  default = 3
+}
+
+/* etcd_node_count must be <= node_count; odd numbers provide quorum */
+variable "etcd_node_count" {
+  default = 3
+}
+
+variable "domain" {
+  default = "kloud3s.io"
+}
+
+variable "hostname_format" {
+  default = "kube%d"
+}
+
+variable "ssh_key_path" {
+  default = "../../.ssh/tf-kube"
+}
+
+variable "ssh_pubkey_path" {
+  default = "../../.ssh/tf-kube.pub"
+}
+
+variable "ssh_keys_dir" {
+  default = "../../.ssh"
+}
+
+variable "k3s_version" {
+  default = "latest"
+}
+
+variable "kubeconfig_path" {
+  default = "../../.kubeconfig"
+}
+
+variable "create_zone" {
+  default = false
+}
+
+variable "cni" {
+  default = "cilium"
+  description = "Choice of CNI to install e.g. flannel, weave, cilium, calico"
+}
+
+variable "overlay_cidr" {
+  default = "10.42.0.0/16"
+  description = "Cluster cidr"
+}
+
+/* hcloud */
+variable "hcloud_token" {
+  default = ""
+}
+
+variable "hcloud_ssh_keys" {
+  type    = list(string)
+  default = [""]
+}
+
+variable "hcloud_location" {
+  default = "nbg1"
+}
+
+variable "hcloud_type" {
+  default = "cx11"
+}
+
+variable "hcloud_image" {
+  default = "ubuntu-18.04"
+}
+
+/* digitalocean */
+variable "digitalocean_token" {
+  default = ""
+}
+
+/* aws dns */
+variable "aws_access_key" {
+  default = ""
+}
+
+variable "aws_secret_key" {
+  default = ""
+}
+
+variable "aws_region" {
+  default = "eu-west-1"
+}
+
+/* cloudflare dns */
+variable "cloudflare_email" {
+  default = ""
+}
+
+variable "cloudflare_api_token" {
+  default = ""
+}
+
+/* google dns */
+variable "google_project" {
+  default = ""
+}
+
+variable "google_region" {
+  default = ""
+}
+
+variable "google_managed_zone" {
+  default = ""
+}
+
+variable "google_credentials_file" {
+  default = ""
+}
