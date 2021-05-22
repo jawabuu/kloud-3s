134ebc268d774e90b670c3f495c925c1de7da4f0
jawabuu <49921621+jawabuu@users.noreply.github.com>
2021-05-22 05:27:42 +0300

    experimental floating ip support for hetzner

--- provider/hcloud/floating_ip.tf
+++ provider/hcloud/floating_ip.tf
@@ -3,6 +3,17 @@ variable "enable_floatingip" {
   description = "Whether to use a floating ip or not"
 }
 
+resource "hcloud_floating_ip" "kloud3s" {
+  count         = var.hosts > 0 && var.enable_floatingip ? 1 : 0
+  type          = "ipv4"
+  home_location = var.location
+}
+
 output "floating_ip" {
-  value = {}
+  # Experimental
+  value = {
+    ip_address    = try(hcloud_floating_ip.kloud3s[0].ip_address, ""),
+    provider      = "hcloud"
+    provider_auth = var.token
+  }
 }
