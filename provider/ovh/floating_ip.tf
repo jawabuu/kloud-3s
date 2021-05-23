ceba0789e3c9fadf6f20381963852edc51bc84d9
jawabuu <49921621+jawabuu@users.noreply.github.com>
2021-05-23 22:01:09 +0300

    add floatingip functionality to ovh

--- provider/ovh/floating_ip.tf
+++ provider/ovh/floating_ip.tf
@@ -3,6 +3,17 @@ variable "enable_floatingip" {
   description = "Whether to use a floating ip or not"
 }
 
+
+resource "openstack_networking_floatingip_v2" "kloud3s" {
+  count = var.hosts > 0 && var.enable_floatingip ? 1 : 0
+  pool  = openstack_networking_network_v2.kube-vpc.name #"Ext-Net"
+}
+
 output "floating_ip" {
-  value = {}
+  # Experimental
+  value = {
+    ip_address    = try(openstack_networking_floatingip_v2.kloud3s[0].address, ""),
+    provider      = "ovh"
+    provider_auth = base64encode("${var.application_key}:${var.application_secret}")
+  }
 }
