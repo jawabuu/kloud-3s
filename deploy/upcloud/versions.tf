4c1baa0c5fdd28f172e3c3b25357970b9e15751c
jawabuu <49921621+jawabuu@users.noreply.github.com>
2021-06-02 13:01:27 +0300

    update providers

--- deploy/upcloud/versions.tf
+++ deploy/upcloud/versions.tf
@@ -1,4 +1,10 @@
 
 terraform {
-  required_version = ">= 0.12.26"
+  required_version = ">= 0.13"
+  required_providers {
+    upcloud = {
+      source  = "UpCloudLtd/upcloud"
+      version = "2.0.0"
+    }
+  }
 }
--- deploy/vultr/versions.tf
+++ deploy/vultr/versions.tf
@@ -1,4 +1,9 @@
-
 terraform {
   required_version = ">= 0.12.26"
+  required_providers {
+    vultr = {
+      source  = "vultr/vultr"
+      version = "~> 1.5.0"
+    }
+  }
 }
