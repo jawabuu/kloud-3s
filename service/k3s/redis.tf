2257d3a74b64707839415c0e506bf868e2fdf1f4
jawabuu <49921621+jawabuu@users.noreply.github.com>
2021-08-05 18:10:01 +0300

    Add redis

---
+++ service/k3s/redis.tf
@@ -0,0 +1,39 @@
+locals {
+  redis = templatefile("${path.module}/templates/redis-helm.yaml", {
+    domain         = var.domain
+    redis_password = random_password.redis_password.result
+  })
+}
+
+resource "random_password" "redis_password" {
+  length  = 10
+  special = false
+}
+
+resource "null_resource" "redis" {
+  count = var.node_count > 0 ? 1 : 0
+  triggers = {
+    k3s_id           = md5(join(" ", null_resource.k3s.*.id))
+    redis            = md5(local.redis)
+    ssh_key_path     = local.ssh_key_path
+    master_public_ip = local.master_public_ip
+  }
+
+  # Use master(s)
+  connection {
+    host        = self.triggers.master_public_ip
+    user        = "root"
+    agent       = false
+    private_key = file(self.triggers.ssh_key_path)
+  }
+
+  # Upload redis
+  provisioner "file" {
+    content     = local.redis
+    destination = "/var/lib/rancher/k3s/server/manifests/redis-helm.yaml"
+  }
+}
+
+output "redis" {
+  value = local.redis
+}
